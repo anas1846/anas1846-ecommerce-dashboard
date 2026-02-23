9155006c683e67f1e652c277ecc1f8802a6af03c
anas1846 <muhammadanas1846@gmail.com>
2026-02-23 23:18:47 +0500

    change

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-
-
 export default defineConfig({
+  base: '/ecommerce-dashboard/',   // NO space
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000,
   },
-   base:"/ecommerce-dashboard/ "
 })
