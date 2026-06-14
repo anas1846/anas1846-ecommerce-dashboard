fcad2e862b0ba5d373388b590887e3c89fa043cc
anas1846 <muhammadanas1846@gmail.com>
2026-06-15 01:25:08 +0500

    Fix Vite base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/ecommerce-dashboard/',   // NO space
+  base: '/anas1846-ecommerce-dashboard/',   // NO space
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000,
